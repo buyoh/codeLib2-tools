ebe48039e7d0e0c59afbecefdad10d471afc0cdf
buyoh <buyoh@users.noreply.github.com>
2024-05-27 09:25:57 +0900

    Base

---
+++ build.rs
@@ -0,0 +1,20 @@
+extern crate schemafy;
+
+// use std::env;
+// use std::path::Path;
+
+fn main() {
+    // TODO
+    // let out_dir = env::var("OUT_DIR").unwrap();
+    // let schema_path = Path::new("path/to/your/schema.json");
+    // let output_path = Path::new(&out_dir).join("schema.rs");
+
+    // schemafy::schemafy(
+    //     schema_path,
+    //     output_path,
+    //     schemafy::RenameRule::PascalCase,
+    //     None,
+    //     None,
+    //     &[]
+    // ).unwrap();
+}
