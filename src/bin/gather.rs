20632f88adacbe81ab318a0c2897fdf76444c15f
buyoh <15198247+buyoh@users.noreply.github.com>
2024-06-03 01:55:10 +0900

    Implement gather

--- src/bin/gather.rs
+++ src/bin/gather.rs
@@ -1,38 +1,44 @@
 use std::fs::File;
-
 use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    // #[arg(short, long)]
-    // name: String,
-
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
-
-    #[arg(long)]  // TODO: for testing. remove this.
-    filepath: String,
+    /// Directory path of the repository
+    #[arg(long)]
+    basepath: String,
+    /// Pretty print JSON
+    #[arg(long, default_value_t = false)]
+    pretty: bool,
 }
 
 fn main() {
     let args = Args::parse();
 
-    // let path = "target/codelib_full.json";
+    let base_path = args.basepath;
 
-    // let codelibs = codelib2_tools::from_json_array(&std::fs::read_to_string(path).unwrap());
+    if !std::path::Path::new(&base_path).is_dir() {
+        eprintln!("{} is not a directory", base_path);
+        std::process::exit(1);
+    }
 
-    // for _ in 0..args.count {
-    //     println!("Hello {}!", args.name)
-    // }
+    let collection = codelib2_tools::gather_collection(&base_path).unwrap();
 
-    // for codelib in codelibs {
-    //     println!("{:?}", codelib);
-    // }
-    let article_path = args.filepath;
-    let file = File::open(&article_path).expect("Failed to open file");
-    let article = codelib2_tools::parse_document_from_file(file, article_path, "rust".to_string(), vec![], vec![]);
-
-    // print article
-    println!("{:?}", article);
+    let mut articles = Vec::new();
+    for (src_paths, lang) in collection.src_paths.iter().zip(collection.langs.iter()) {
+        for src_path in src_paths {
+            let total_path = collection.complete_path_str(src_path);
+            let file = File::open(&total_path).expect("Failed to open file");
+            let commits = vec![];  // TODO:
+            let tested_by = vec![];  // TODO:
+            let article = codelib2_tools::parse_document_from_file(file, src_path.clone(), lang.clone(), commits, tested_by);
+            articles.push(article.unwrap());
+        }
+    }
+    let json_str = if args.pretty {
+        serde_json::to_string_pretty(&articles).unwrap()
+    } else {
+        serde_json::to_string(&articles).unwrap()
+    };
+    print!("{}", json_str);
 }
