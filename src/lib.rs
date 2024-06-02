e0c582a60de883ccb7c8879a0c7d278751d9ff01
buyoh <15198247+buyoh@users.noreply.github.com>
2024-06-02 23:59:54 +0900

    Implement repo_collector (partially)

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 mod codelib;
 mod parser;
+mod repo_collector;
 
-use chrono::{DateTime, FixedOffset};
 pub use codelib::Article;
 
 pub use parser::parse_document_from_file;
-
+pub use repo_collector::gather_collection;
+pub use repo_collector::gather_commit_info;
---
+++ src/repo_collector/mod.rs
@@ -0,0 +1,78 @@
+use glob::glob;
+
+// TODO: hidden?
+#[derive(Debug)]
+pub struct Collection {
+    pub base_path: String,
+    pub src_paths: Vec<String>,
+    pub test_paths: Vec<String>,
+    pub langs: Vec<String>,
+}
+
+fn collect_langs(base_path: &str) -> Result<Vec<String>, String> {
+    let mut langs = Vec::new();
+    let g = if let Ok(g) = glob(&format!("{}/src/*", base_path)) {
+        g
+    } else {
+        return Err("Failed to read glob pattern; base_path may be wrong".to_string());
+    };
+    for entry in g {
+        // if entry is directory
+        if let Ok(entry) = entry {
+            if entry.is_dir() {
+                let lang = entry.file_name().unwrap().to_str().unwrap().to_string();
+                langs.push(lang);
+            }
+        }
+    }
+    Ok(langs)
+}
+
+// TODO: Remove panic
+fn collect_paths(base_path: &str, langs: &Vec<String>, src_or_test: &str) -> Result<Vec<String>, String> {
+    let mut src_paths = Vec::new();
+    for lang in langs {
+        let g = if let Ok(g) = glob(&format!("{}/{}/{}/**/*", base_path, src_or_test, lang)) {
+            g
+        } else {
+            return Err("Failed to read glob pattern; base_path may be wrong".to_string());
+        };
+        for entry in g {
+            if let Ok(entry) = entry {
+                // if entry is file
+                if entry.is_file() {
+                    src_paths.push(entry.to_str().unwrap().to_string());
+                }
+            }
+        }
+    }
+    Ok(src_paths)
+}
+
+pub fn gather_collection(base_path: &str) -> Result<Collection, String> {
+    let langs = match collect_langs(base_path) {
+        Ok(langs) => langs,
+        Err(err) => return Err(err),
+    };
+    let src_paths = match collect_paths(base_path, &langs, "src") {
+        Ok(src_paths) => src_paths,
+        Err(err) => return Err(err),
+    };
+    let test_paths = match collect_paths(base_path, &langs, "test") {
+        Ok(test_paths) => test_paths,
+        Err(err) => return Err(err),
+    };
+    
+    let collection = Collection {
+        base_path: base_path.to_string(),
+        langs: langs.clone(),
+        src_paths,
+        test_paths,
+    };
+    Ok(collection)
+}
+
+pub fn gather_commit_info(_filepath: &str) -> Result<Vec<crate::codelib::Commit>, String> {
+    // TODO: implement
+    Ok(Vec::new())
+}
